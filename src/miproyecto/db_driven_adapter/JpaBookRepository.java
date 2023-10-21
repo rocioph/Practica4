276e945d2d17e6ad284910793bb1dc137e74aa40
rocioph <rocioph70@gmail.com>
2023-10-21 13:57:11 +0200

    punto 10 implementado

---
+++ src/miproyecto/db_driven_adapter/JpaBookRepository.java
@@ -0,0 +1,25 @@
+package miproyecto.db_driven_adapter;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import miproyecto.core.domain.Book;
+import miproyecto.core.domain.BookDoesNotExistException;
+import miproyecto.core.driven_ports.BookRepository;
+import miproyecto.db_driven_adapter.domain.BookEntity;
+import miproyecto.db_driven_adapter.jparepository.HAAJpaRepository;
+
+@Component
+public class JpaBookRepository implements BookRepository {
+   
+@Autowired //enlaza repositorio y servicio
+private HAAJpaRepository haaJpaRepository;
+    @Override
+public Book findById(Long id) {
+Optional<BookEntity> bookEntityOptional = haaJpaRepository.findById(id);
+BookEntity bookEntity = bookEntityOptional.orElseThrow(BookDoesNotExistException::new);
+return bookEntity.toBook();
+}
+}
